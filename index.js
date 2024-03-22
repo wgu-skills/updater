9531ab355e96cf4fb3b144338db048cd1f6f790d
David Petersen <dp@davidp.us>
2024-03-22 11:28:04 -0600

    Refactor index.js to use ES6 imports and update error handling

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import * as github from '@actions/github';
+
 // index.js
-const core = require('@actions/core');
-const exec = require('@actions/exec');
-const github = require('@actions/github');
 
 async function run() {
-  try {
-    const skillCollectionUrl = core.getInput('skillCollectionUrl');
-    const patToken = core.getInput('patToken');
+    try {
+        const skillCollectionUrl = core.getInput('skillCollectionUrl');
+        const patToken = core.getInput('patToken');
 
-    // Perform your actions here, e.g., fetch data, git operations, etc.
-    // Use the `exec` module to run shell commands if necessary
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+        // Perform your actions here, e.g., fetch data, git operations, etc.
+        // Use the `exec` module to run shell commands if necessary
+
+        
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
 run();
