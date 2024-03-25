d5bf45e189b8ee3b74015b922a7c404a25a3817c
David Petersen <dp@davidp.us>
2024-03-25 10:43:15 -0600

    Refactor collection export and file creation

--- index.js
+++ index.js
@@ -11,20 +11,21 @@ async function run() {
 
 		 const collection = await getCollection(config.collection.uuid, config.collection.slug);
 
+		console.log('Collection:', collection);
 		// Get the collection
-		await Promise.all([
-			collection.export(FORMAT_JSON), // Export the collection
-			collection.export(FORMAT_YAML), // Export the collection
-			collection.exportSkills(FORMAT_JSON), // Export the skills
-			collection.exportSkills(FORMAT_YAML), // Export the skills
-			collection.createIndexFile(), // Create the index file
-			collection.createPackageJsonFile(), // Create the package.json file
-			collection.createReadmeFile() // Create the README file
-		]);
+		// await Promise.all([
+		// 	collection.export(FORMAT_JSON), // Export the collection
+		// 	collection.export(FORMAT_YAML), // Export the collection
+		// 	collection.exportSkills(FORMAT_JSON), // Export the skills
+		// 	collection.exportSkills(FORMAT_YAML), // Export the skills
+		// 	collection.createIndexFile(), // Create the index file
+		// 	collection.createPackageJsonFile(), // Create the package.json file
+		// 	collection.createReadmeFile() // Create the README file
+		// ]);
 
 		// Check if the directory is a git repository
 		// collection.updateRepo()
-		
+
 	} catch (error) {
 		setFailed(error.message);
 	}
