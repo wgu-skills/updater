8f6d08230e1237f4cb7445b6a9782b2e33a47644
David Petersen <dp@davidp.us>
2024-03-25 22:51:05 -0600

    Refactor fileOperations.js for improved readability and maintainability

--- helpers/fileOperations.js
+++ helpers/fileOperations.js
@@ -2,34 +2,35 @@ import fs from 'fs/promises';
 import path from 'path';
 import { toCamelCase } from './stringOperations.js';
 
-const FORMAT_JSON = "json";
-const FORMAT_YAML = "yaml";
+const FORMAT_JSON = 'json';
+const FORMAT_YAML = 'yaml';
 
 const FILE_EXTENSIONS = {
-  skillJson: ".skill.json",
-  skillYaml: ".skill.yaml"
+	skillJson: '.skill.json',
+	skillYaml: '.skill.yaml',
 };
 
 // Utility functions
-const getFilePath = (fileName) => path.join(process.env.GITHUB_WORKSPACE, fileName);
+const getFilePath = (fileName) =>
+	path.join(process.env.GITHUB_WORKSPACE, fileName);
 
 const writeToFile = async (filePath, content) => {
-  await fs.mkdir(path.dirname(filePath), { recursive: true });
-  await fs.writeFile(filePath, content);
+	await fs.mkdir(path.dirname(filePath), { recursive: true });
+	await fs.writeFile(filePath, content);
 };
 
 const listFiles = async (dir) => fs.readdir(dir);
 
 // Collection File Creators
 const createFileFromTemplate = async (collection, fileType, contentBuilder) => {
-  try {
-    const filePath = getFilePath(`${collection.slug}/${fileType}`);
-    const content = await contentBuilder(collection);
-    await writeToFile(filePath, content);
-  } catch (error) {
-    console.error(`Error creating ${fileType}:`, error);
-    throw error;
-  }
+	try {
+		const filePath = getFilePath(`${collection.slug}/${fileType}`);
+		const content = await contentBuilder(collection);
+		await writeToFile(filePath, content);
+	} catch (error) {
+		console.error(`Error creating ${fileType}:`, error);
+		throw error;
+	}
 };
 
 // const createIndexFile = async (collection) => {
@@ -50,47 +51,65 @@ const createFileFromTemplate = async (collection, fileType, contentBuilder) => {
 //   });
 // };
 const createIndexFile = async (collection) => {
-  await createFileFromTemplate(collection, 'index.js', async (collection) => {
-    const imports = [], exports = [];
-    
-    collection.skills.forEach(skillName => {
-      const variableName = toCamelCase(skillName);
-      imports.push(`import ${variableName} from './skills/${skillName}${FILE_EXTENSIONS.skillJson}';`);
-      exports.push(variableName);
-    });
+	await createFileFromTemplate(collection, 'index.js', async (collection) => {
+		const imports = [],
+			exports = [];
 
-    return `${imports.join("\n")}\n\nexport { ${exports.join(", ")} };`;
-  });
+		collection.skills.forEach((skillName) => {
+			console.log('skillName', skillName);
+			const variableName = toCamelCase(skillName);
+			imports.push(
+				`import ${variableName} from './skills/${skillName}${FILE_EXTENSIONS.skillJson}';`
+			);
+			exports.push(variableName);
+		});
+
+		return `${imports.join('\n')}\n\nexport { ${exports.join(', ')} };`;
+	});
 };
 
-
 const createPackageJsonFile = async (collection) => {
-  await createFileFromTemplate(collection, 'package.json', (collection) => JSON.stringify({
-    name: collection.slug,
-    version: collection.version,
-    description: collection.description,
-    main: "index.js",
-    scripts: { test: 'echo "Error: no test specified" && exit 1' },
-    author: "Western Governors University",
-    license: "https://creativecommons.org/licenses/by-sa/4.0/"
-  }, null, 4));
+	await createFileFromTemplate(collection, 'package.json', (collection) =>
+		JSON.stringify(
+			{
+				name: collection.slug,
+				version: collection.version,
+				description: collection.description,
+				main: 'index.js',
+				scripts: { test: 'echo "Error: no test specified" && exit 1' },
+				author: 'Western Governors University',
+				license: 'https://creativecommons.org/licenses/by-sa/4.0/',
+			},
+			null,
+			4
+		)
+	);
 };
 
 const createReadmeFile = async (collection) => {
-  await createFileFromTemplate(collection, 'README.md', async (collection) => {
-    const readmeHeader = `# ${collection.name}\n\n${collection.description}\n\n## Skills\n\n`;
-    const skillsPath = getFilePath(`${collection.slug}/skills`);
-    const skillFiles = await listFiles(skillsPath);
+	await createFileFromTemplate(collection, 'README.md', async (collection) => {
+		const readmeHeader = `# ${collection.name}\n\n${collection.description}\n\n## Skills\n\n`;
+		const skillsPath = getFilePath(`${collection.slug}/skills`);
+		const skillFiles = await listFiles(skillsPath);
 
-    const skillLinks = await Promise.all(skillFiles
-      .filter(file => file.endsWith(FILE_EXTENSIONS.skillJson))
-      .map(async (file) => {
-        const skillName = path.basename(file, FILE_EXTENSIONS.skillJson);
-        return `- ${skillName} [JSON](./skills/${skillName}${FILE_EXTENSIONS.skillJson}) [YAML](./skills/${skillName}${FILE_EXTENSIONS.skillYaml})`;
-      }));
+		const skillLinks = await Promise.all(
+			skillFiles
+				.filter((file) => file.endsWith(FILE_EXTENSIONS.skillJson))
+				.map(async (file) => {
+					const skillName = path.basename(file, FILE_EXTENSIONS.skillJson);
+					return `- ${skillName} [JSON](./skills/${skillName}${FILE_EXTENSIONS.skillJson}) [YAML](./skills/${skillName}${FILE_EXTENSIONS.skillYaml})`;
+				})
+		);
 
-    return `${readmeHeader}${skillLinks.join("\n")}`;
-  });
+		return `${readmeHeader}${skillLinks.join('\n')}`;
+	});
 };
 
-export { writeToFile, createIndexFile, createPackageJsonFile, createReadmeFile, FORMAT_JSON, FORMAT_YAML};
+export {
+	writeToFile,
+	createIndexFile,
+	createPackageJsonFile,
+	createReadmeFile,
+	FORMAT_JSON,
+	FORMAT_YAML,
+};
