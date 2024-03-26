f11a2196bc0206c667150b766403c00e9a354b60
David Petersen <dp@davidp.us>
2024-03-26 09:30:50 -0600

    Add missing closing brace in toCamelCase function

--- helpers/stringOperations.js
+++ helpers/stringOperations.js
@@ -13,6 +13,7 @@ const toCamelCase = (fileName) => {
     .join('')
     .replace(/[^a-zA-Z0-9]/g, '')
     .replace(/^./, (str) => str.toLowerCase());
+}
 
 const createSlug = (name) => slugify(name, { lower: true, strict: true });
 
