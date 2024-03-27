26f733fb5dc1009f978d99b44db7d9346053e6c5
David Petersen <dp@davidp.us>
2024-03-27 10:39:02 -0600

    Refactor exportCollection function and improve error handling

--- index.js
+++ index.js
@@ -4,27 +4,26 @@ import SkillCollection from './types/SkillCollection';
 import { FORMAT_JSON } from './helpers/fileOperations';
 
 async function exportCollection(collection) {
-    await Promise.all([
-        collection.export(FORMAT_JSON),
-        collection.exportSkills(FORMAT_JSON),
-        collection.createIndexFile(),
-        collection.createPackageJsonFile(),
-        collection.createReadmeFile()
-    ]);
+	await Promise.all([
+		collection.export(FORMAT_JSON),
+		collection.exportSkills(FORMAT_JSON),
+		collection.createIndexFile(),
+		collection.createPackageJsonFile(),
+		collection.createReadmeFile(),
+	]);
 }
 
 async function run() {
-    try {
-		
-        const collection = await SkillCollection.fetchAndCreate(config.collection.uuid, config.collection.slug);
-        await exportCollection(collection);
-
-    } catch (error) {
-
-        console.error('Error occurred:', error); // Detailed error logging
-        setFailed(error.message);
-
-    }
+	try {
+		const collection = await SkillCollection.fetchAndCreate(
+			config.collection.uuid,
+			config.collection.slug
+		);
+		await exportCollection(collection);
+	} catch (error) {
+		console.error('Error occurred:', error); // Detailed error logging
+		setFailed(error.message);
+	}
 }
 
 run();
--- types/SkillCollection.js
+++ types/SkillCollection.js
@@ -3,54 +3,66 @@ import yaml from 'js-yaml';
 import config from '../helpers/config.js';
 import Skill from './Skill.js';
 import {
-  writeToFile, createIndexFile, createPackageJsonFile, createReadmeFile, FORMAT_JSON
+	writeToFile,
+	createIndexFile,
+	createPackageJsonFile,
+	createReadmeFile,
+	FORMAT_JSON,
 } from '../helpers/fileOperations.js';
 
 class SkillCollection {
-  constructor(data) {
-    this.slug = config.collection.slug;
-    this.skills = data.skills.map(skill => new Skill(skill));
-  }
+	constructor(data) {
+		this.slug = config.collection.slug;
+		this.skills = data.skills.map((skill) => new Skill(skill));
+	}
 
-  static async fetchAndCreate(uuid, slug) {
-    const url = `https://aa-skill.wgu.edu/api/collections/${uuid}`;
-    const response = await fetch(url, { headers: { Accept: 'application/json' } });
+	static async fetchAndCreate(uuid, slug) {
+		const url = `https://aa-skill.wgu.edu/api/collections/${uuid}`;
+		const response = await fetch(url, {
+			headers: { Accept: 'application/json' },
+		});
+
+		if (!response.ok) {
+			throw new Error(`Network response not ok: ${response.status}`);
+		}
+
+		const json = await response.json();
+		return new SkillCollection({ ...json, slug });
+	}
+
+	async export(format) {
     
-    if (!response.ok) {
-      throw new Error(`Network response not ok: ${response.status}`);
-    }
+		const formattedCollection = {
+			...this,
+			skills: this.skills.map((skill) => {
+				const { slug, ...rest } = skill.get();
+				return rest;
+			}),
+		};
 
-    const json = await response.json();
-    return new SkillCollection({ ...json, slug });
-  }
+		const dataToWrite =
+			format === FORMAT_JSON
+				? JSON.stringify(formattedCollection, null, 4)
+				: yaml.dump(formattedCollection);
 
-  async export(format) {
-    const formattedCollection = {
-      ...this,
-      skills: this.skills.map(skill => skill.get())
-    };
-    const dataToWrite = format === FORMAT_JSON
-      ? JSON.stringify(formattedCollection, null, 4)
-      : yaml.dump(formattedCollection);
-    
-    await writeToFile(`./collection.skill.${format}`, dataToWrite);
-  }
+		await writeToFile(`./collection.skill.${format}`, dataToWrite);
+	}
 
-  async exportSkills(format) {
-    await Promise.all(this.skills.map(skill => skill.export(format)));
-  }
+	async exportSkills(format) {
+		await Promise.all(this.skills.map((skill) => skill.export(format)));
+	}
 
-  async createIndexFile() {
-    await createIndexFile(this);
-  }
+	async createIndexFile() {
+		await createIndexFile(this);
+	}
 
-  async createPackageJsonFile() {
-    await createPackageJsonFile(this);
-  }
+	async createPackageJsonFile() {
+		await createPackageJsonFile(this);
+	}
 
-  async createReadmeFile() {
-    await createReadmeFile(this);
-  }
+	async createReadmeFile() {
+		await createReadmeFile(this);
+	}
 }
 
 export default SkillCollection;
