0bc3d4d4eb99a33e4ae57fbe5dba28c334e7d4eb
David Petersen <dp@davidp.us>
2024-03-28 13:54:48 -0600

    Refactor createCollectionJsonFile.js

--- helpers/createCollectionJsonFile.js
+++ helpers/createCollectionJsonFile.js
@@ -1,24 +1,23 @@
-import { FORMAT_JSON } from './fileOperations';
-import { writeToFile } from './fileOperations';
-import yaml from 'js-yaml';
+import { writeToFile } from './fileOperations'
+import yaml from 'js-yaml'
+import { FORMAT_JSON } from './fileOperations'
 
 const createCollectionJsonFile = async (collection, format) => {
   // Extracting only necessary properties from each skill
   const formattedCollection = {
     ...collection,
     skills: collection.skills.map((skill) => {
-      const { slug, ...rest } = skill.get();
-      return rest;
+      const { slug, ...rest } = skill.get()
+      return rest
     })
-  };
+  }
 
   // Choose the format based on the input parameter
-  const dataToWrite = format === FORMAT_JSON 
-    ? JSON.stringify(formattedCollection, null, 4) 
-    : yaml.dump(formattedCollection);
+  const dataToWrite =
+    format === FORMAT_JSON ? JSON.stringify(formattedCollection, null, 4) : yaml.dump(formattedCollection)
 
   // Write the formatted data to a file
-  await writeToFile(`./collection.skill.${format}`, dataToWrite);
-};
+  await writeToFile(`./collection.skill.${format}`, dataToWrite)
+}
 
-export default createCollectionJsonFile;
+export default createCollectionJsonFile
